c61af56423b794a5c3d8fb2c6cbb1cd38fce11ba
Anik Zod <anikdas169@gmail.com>
2025-07-15 16:59:02 +0600

    added frontend link to cors

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.use(helmet());
 app.use(compression())
 app.use(morgan('dev'))
 app.use(cors({
-  origin:'http://localhost:5173',
+  origin:'https://gorcery-delivery-app-pyq7.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
   allowedHeaders: ['Content-Type','Authorization']
